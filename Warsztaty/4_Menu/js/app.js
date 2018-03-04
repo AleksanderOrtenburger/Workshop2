70ea37da78e83ef4e2b0182817d13a542e01a9a0
AleksanderOrtenburger <olek.ortenburger@gmail.com>
2018-03-04 16:29:26 +0100

    task4 done

--- Warsztaty/4_Menu/js/app.js
+++ Warsztaty/4_Menu/js/app.js
@@ -1,3 +1,20 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function(){
+    var mainLi = document.querySelectorAll(".nav>ul>li");
+
+    mainLi.forEach(function(el){
+        el.style.cursor = "pointer";
+        el.style.border = "1px solid grey";
+        el.style.borderRadius = "5px";
+        el.addEventListener("mouseover", function(event){
+            var ul = el.querySelector("ul");
+            ul.style.display = "block";
+
+        });
+        el.addEventListener("mouseout", function(event){
+            var ul = el.querySelector("ul");
+            ul.style.display = "none";
+
+        });
+    });
+
+});
