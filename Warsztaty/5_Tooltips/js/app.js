34b7dc9a0c5dcf5f96ebc27cb7317981f4d31ece
AleksanderOrtenburger <olek.ortenburger@gmail.com>
2018-03-07 23:00:34 +0100

    workshop2 done

--- Warsztaty/5_Tooltips/js/app.js
+++ Warsztaty/5_Tooltips/js/app.js
@@ -1,8 +1,21 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function(){
+    var tools = document.querySelectorAll(".tooltip");
 
 
-/*
- <span class="tooltipText">Text tooltipa</span>
- */
+    tools.forEach(function(element){
+        var toolDis = document.createElement("span");
+        toolDis.classList.add("tooltipText");
+        element.appendChild(toolDis);
+        toolDis.innerText = element.getAttribute("data-text");
+        element.style.cursor = "pointer";
+        toolDis.style.display = "none";
+    });
+       tools.forEach(function(element){
+        element.addEventListener("mouseover", function(e){
+            this.children[0].style.display = "block";
+        });
+        element.addEventListener("mouseout", function(e){
+            this.children[0].style.display = "none";
+        });
+    });
+});
--- Warsztaty/6_ToDoList/js/app.js
+++ Warsztaty/6_ToDoList/js/app.js
@@ -1,14 +1,75 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener('DOMContentLoaded', function() {
+
+    var counter = 0;
+    var addBtn = document.querySelector('#addTaskButton');
+    var firstIn = document.querySelector('#taskInput');
+    var secondIn = document.querySelector('#priorInput');
+    var list = document.querySelector('#taskList');
+    var remAll = document.querySelector('#removeFinishedTasksButton');
+    var toDo= document.querySelector('#count');
 
 
+    function unfinishedTaskNum(num) {
+        return toDo.innerHTML = "" + num;
+    }
+
+    addBtn.addEventListener('click', function() {
+
+        counter++;
+
+        var li = document.createElement('li');
+        var h1 = document.createElement('h1');
+        var delButton = document.createElement('button');
+        var comButton = delButton.cloneNode(false);
+
+        h1.innerHTML = 'Task ' +counter+ ' : ' +firstIn.value;
+
+        delButton.innerHTML = 'Delete';
+
+        comButton.innerHTML = 'Complete';
+
+        li.appendChild(h1);
+        li.appendChild(delButton);
+        li.appendChild(comButton);
+
+        if(firstIn.value.length > 5 && firstIn.value.length < 100) {
+            list.appendChild(li);
+            firstIn.value = null;
+        }
+
+        function result() {
+            var done = document.querySelectorAll('.done');
+            return list.children.length - done.length;
+        }
+
+        unfinishedTaskNum(result());
+
+        comButton.addEventListener('click', function() {
+            this.parentElement.classList.toggle('done');
+            h1.style.color='red';
+
+            unfinishedTaskNum(result());
+        });
+
+        delButton.addEventListener('click', function() {
+            li.parentElement.removeChild(li);
+
+            unfinishedTaskNum(result());
+        });
 
 
+    });
 
+    remAll.addEventListener('click', function() {
 
+        var taskFin = document.querySelectorAll('.done');
 
+        [...taskFin].forEach(function(el) {
+            el.parentElement.removeChild(el);
+        });
 
+        unfinishedTaskNum(list.children.length);
 
+    })
 
-
+});
